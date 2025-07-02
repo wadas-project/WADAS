35ba74631a7fc706267739017c2bbba39738f216
Stefano Dell'Osa <stefano.dellosa@gmail.com>
2025-07-02 18:40:18 +0200

    Bugfix to properly enable deterrent actuator in WADAS

--- wadas/domain/actuator_server_app.py
+++ wadas/domain/actuator_server_app.py
@@ -36,12 +36,12 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"])
 @app.get("/api/v1/actuators/{actuator_id}")
 async def get_actuator_command(actuator_id: str):
     """Method to give a command to an actuator when requested."""
-    logger.info("Connected remote actuator with ID: %s", actuator_id)
+    logger.debug("Connected remote actuator with ID: %s", actuator_id)
 
     if actuator_id in Actuator.actuators:
         cmd = Actuator.actuators[actuator_id].get_command()
         return JSONResponse(content=json.loads(cmd) if cmd else {"cmd": None}, status_code=200)
 
     else:
-        logger.info("No actuator found with ID: %s", actuator_id)
+        logger.error("No actuator found with ID: %s", actuator_id)
         raise HTTPException(status_code=404, detail="Actuator does not exist")
