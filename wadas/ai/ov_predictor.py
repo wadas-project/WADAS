7cf01859f483b1162f4ba96b67190d8e2e6c6db2
Alessandro Palla <alespalla.ap@gmail.com>
2025-07-03 16:10:16 +0200

    Enhance OVBackend to handle encrypted models and load metadata for improved inference configuration

--- wadas/ai/ov_predictor.py
+++ wadas/ai/ov_predictor.py
@@ -5,7 +5,12 @@ from pathlib import Path
 import torch
 import wadas_runtime as wadas
 from ultralytics.models.yolo.detect import DetectionPredictor
-from ultralytics.nn.autobackend import AutoBackend
+from ultralytics.nn.autobackend import (
+    AutoBackend,
+    check_class_names,
+    default_class_names,
+)
+from ultralytics.utils import yaml_load
 from ultralytics.utils.torch_utils import select_device
 
 from wadas.ai.openvino_model import __model_folder__
@@ -20,7 +25,7 @@ def load_ov_model(weights, device, inference_mode="LATENCY"):
     if not w.is_file():  # if not *.xml
         w = next(w.glob("*.xml"))  # get *.xml file from *_openvino_model dir
     config = {"PERFORMANCE_HINT": inference_mode}
-    return wadas.load_and_compile_model(str(w), str(w.with_suffix(".bin")), device, config)
+    return wadas.load_and_compile_model(str(w), str(w.with_suffix(".bin")), device.upper(), config)
 
 
 class OVBackend(AutoBackend):
@@ -37,13 +42,63 @@ class OVBackend(AutoBackend):
         fuse=True,
         verbose=True,
     ):
-        super().__init__(weights, torch.device(device), dnn, data, fp16, batch, fuse, verbose)
+        """AutoBackend class attempts to load a model using OpenVINO runtime.
+        However for an encrypted model that won't work because some layer attributes like shapes
+        and so are in the bin and encrypted, so if AutoBackend loads it it will fail.
+        The solution here is to bypass the AutoBackend __init__ and load the model here.
+        AutoBakend sets a lot of attributes that we need to set here manually."""
+        torch.nn.Module.__init__(self)
+        nhwc = False  # default to NCHW
+        dynamic = False  # default to static shape
+        fp16 = fp16
         w = str(weights[0] if isinstance(weights, list) else weights)
+        nn_module = False
         w = Path(w)
+        (
+            pt,
+            jit,
+            onnx,
+            xml,
+            engine,
+            coreml,
+            saved_model,
+            pb,
+            tflite,
+            edgetpu,
+            tfjs,
+            paddle,
+            mnn,
+            ncnn,
+            imx,
+            rknn,
+            triton,
+        ) = self._model_type(w)
         if not w.is_file():  # if not *.xml
             w = next(w.glob("*.xml"))  # get *.xml file from *_openvino_model dir
-        self.inference_mode = "LATENCY"
-        self.ov_compiled_model = load_ov_model(w, ov_device, self.inference_mode)
+        inference_mode = "LATENCY"
+        ov_compiled_model = load_ov_model(w, ov_device, inference_mode)
+        input_name = ov_compiled_model.input().get_any_name()
+
+        metadata = yaml_load(w.parent / "metadata.yaml")
+        if metadata and isinstance(metadata, dict):
+            for k, v in metadata.items():
+                if k in {"stride", "batch"}:
+                    metadata[k] = int(v)
+                elif k in {"imgsz", "names", "kpt_shape", "args"} and isinstance(v, str):
+                    metadata[k] = eval(v)
+            stride = metadata["stride"]
+            task = metadata["task"]
+            batch = metadata["batch"]
+            imgsz = metadata["imgsz"]
+            names = metadata["names"]
+            kpt_shape = metadata.get("kpt_shape")
+            end2end = metadata.get("args", {}).get("nms", False)
+            dynamic = metadata.get("args", {}).get("dynamic", dynamic)
+
+        if "names" not in locals():  # names missing
+            names = default_class_names(data)
+        names = check_class_names(names)
+        self.__dict__.update(locals())
 
 
 class OVPredictor(DetectionPredictor):
